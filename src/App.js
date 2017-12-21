8a6d0ef5eea2941de6a1c9eac01d190c72058d37
uidevperson <32483505+uidevperson@users.noreply.github.com>
2017-12-21 17:02:59 +0000

    start setting up router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
-
+const ABOUT = () => <h5>about</h5>
 const HOME = () => <h1>HOME</h1>
 
 const App = () => (
   <Router>
-    <Route path="/" component={ HOME }/>
+    <div>
+      <Route path="/about" component={ ABOUT}/>
+      <Route exact path="/" component={ HOME }/>
+    </div>
   </Router>
 
 );
